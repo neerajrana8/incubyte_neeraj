a3c185f35d16a0c68b41e60582900910a720e2e8
Neeraj Rana <neeraj@getcovered.io>
2024-07-03 17:19:31 +0530

    Inistial tests

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
 require 'rspec'
-
+require '../lib/string_calculator'
 describe "It should calculate sum of the number in a string" do
 
+  context "Check .add method" do
+    it "should return 0 if empty string is provide" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+  end
 end
