de34b2c82bbda37d76ee1e8e4c0ebab1f2946a41
Neeraj Rana <neeraj@getcovered.io>
2024-07-03 17:31:43 +0530

    added few more tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,8 @@
 class StringCalculator
 
+  def self.add(input_str)
+    return 0 if input_str.empty?
+    input_str.to_i  if input_str.length == 1
+  end
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,14 @@
 require 'rspec'
-require '../lib/string_calculator'
+require './lib/string_calculator'
 describe "It should calculate sum of the number in a string" do
 
   context "Check .add method" do
     it "should return 0 if empty string is provide" do
       expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it "should return 4" do
+      expect(StringCalculator.add("4")).to eq(4)
     end
 
   end
