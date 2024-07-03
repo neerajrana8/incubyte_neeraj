6324d660876800aea0f62cbbd6a90033f90f0ded
Neeraj Rana <neeraj@getcovered.io>
2024-07-03 18:39:33 +0530

    finished all the functionality

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,11 +2,21 @@ class StringCalculator
 
   def self.add(input_str)
     return 0 if input_str.empty?
-
-    input_str = input_str.gsub("\n", ",")
+    #//:\n2:3:4:5:6
+    # 2,3,4,5,6
+    original_delimiter = ","
+    if input_str.start_with?("//")
+      str =  input_str.split("\n")
+      input_str = str.last.gsub(str.first[2], original_delimiter)
+      input_str = input_str.gsub(str.first, "")
+    end
+    input_str = input_str.gsub("\n", original_delimiter)
     input_array = input_str.split(",").map(&:to_i)
+    negative_list =  input_array.select{ |num| num < 0 }
+    unless negative_list.empty?
+      raise "Negative number not allowed: #{negative_list.join(",")}"
+    end
     input_array.sum
-
   end
 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,21 +3,34 @@ require './lib/string_calculator'
 describe "It should calculate sum of the number in a string" do
 
   context "Check .add method" do
-    it "should return 0 if empty string is provide" do
-      expect(StringCalculator.add("")).to eq(0)
+    context " Check for postive number and other characters" do
+      it "should return 0 if empty string is provide" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+
+      it "should return 4" do
+        expect(StringCalculator.add("4")).to eq(4)
+      end
+
+      it "should return 6 for input 1,5" do
+        expect(StringCalculator.add("1,5")).to eq(6)
+      end
+
+      it "should return sum for string with new line operator" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+
+      it "should return sum for custom delimiter" do
+        expect(StringCalculator.add("//:\n2:3:4")).to eq(9)
+      end
     end
 
-    it "should return 4" do
-      expect(StringCalculator.add("4")).to eq(4)
+    context "It should raise error on negative numbers" do
+      it "should not process negative numbers" do
+        expect(StringCalculator.add("1,-1,-2,4")).to raise_error(RuntimeError)
+        expect(StringCalculator.add("1,-1,-2,4")).to raise_error("Negative number not allowed: -1,-2")
+      end
     end
-
-    it "should return 6 for input 1,5" do
-      expect(StringCalculator.add("1,5")).to eq(6)
-    end
-
-    it "should return sum for string with new line operator" do
-      expect(StringCalculator.add("1\n2,3")).to eq(6)
-    end
-
   end
+
 end
